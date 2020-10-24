9b97981e1f7940ee0fabe06b6e2a05f7f2cad163
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-10-24 16:01:50 -0500

    Quick accordion

---
+++ scripts/accordion.js
@@ -0,0 +1,15 @@
+//bare bones accordion functionality
+(function accordionInit() {
+    const buttons = document.getElementsByClassName("js-accordion-button");
+    console.log(buttons);
+    const toggle = function() {
+        if (this.getAttribute("aria-expanded") === "true") {
+            this.setAttribute("aria-expanded","false");
+        } else {
+            this.setAttribute("aria-expanded","true");
+        }
+    }
+    for (let button of buttons) {
+        button.addEventListener("click",toggle);
+    }
+}());
