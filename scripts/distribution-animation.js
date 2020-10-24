b2fd57addca56fdfff90ee0446f01bba7240f047
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-10-24 17:11:58 -0500

    Content image block and quotation styling, added more diagrams, animation for distribution diagram

---
+++ scripts/distribution-animation.js
@@ -0,0 +1,19 @@
+(function distributionInit() {
+    const yellowLines = document.getElementById("yellow-lines");
+    const blackLines = document.getElementById("black-lines");
+    const blueLines = document.getElementById("blue-lines");
+    console.log(blueLines);
+    function animateLines(collection) {
+        const lines = collection.getElementsByTagName("path");
+        //create a greensock timeline that repeats forever
+        const tl = gsap.timeline({repeat: -1, repeatDelay: 2});
+
+        //add each individual line to the timeline
+        for (line of lines) {
+            tl.to(line, {duration: .01, opacity: 1});
+        };
+    }
+    animateLines(blueLines);
+    animateLines(blackLines);
+    animateLines(yellowLines);
+})();
